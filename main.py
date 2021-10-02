24e2602a4fb10722d394acc74e596bc9ff77c0c1
aminGhafoory <74732606+aminGhafoory@users.noreply.github.com>
2021-10-02 17:44:47 +0330

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ api_key = os.environ['api_key']
 cx = os.environ['cx']
 query ='bear'
 
-def google_image_search(api_key: str,cx: str ,query: str)->None:
+def google_image_search(api_key: str,cx: str ,query: str) -> None:
 
-    for page in tqdm(range(1,2)):
+    for page in tqdm(range(1,10)):
 
         start = (page-1)*10 +1
         url = f'https://customsearch.googleapis.com/customsearch/v1?cx={cx}&safe=off&searchType=image&key={api_key}&q={query}&start={start}'
@@ -21,13 +21,5 @@ def google_image_search(api_key: str,cx: str ,query: str)->None:
             for i in links:
                 f.write(f'{i}\n')
 
-def save_image_from_url(url):
-    img_name = url.split('/')[-1]
-    r = requests.get(url, allow_redirects = True)
-    with open(img_name,'wb') as f:
-        f.write(r.content)
-
-
 
 google_image_search(api_key,cx,query)
-url_list  = [line.rstrip('\n') for line in open("output.txt",'r')]
